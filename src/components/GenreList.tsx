7c80f76cc442f8a64d4b5ad744f0139e7bdc0684
jebarcha <jebarcha@hotmail.com>
2023-09-22 15:56:37 -0600

    Shoq a spinner while fetching the genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,9 +1,12 @@
-import { HStack, List, ListItem, Text, Image } from '@chakra-ui/react';
+import { HStack, List, ListItem, Text, Image, Spinner } from '@chakra-ui/react';
 import useGenres from '../hooks/useGenres';
 import getCroppedImageUrl from '../services/image-url';
 
 const GenreList = () => {
-  const { data: genres } = useGenres();
+  const { data: genres, isLoading, error } = useGenres();
+
+  if (error) return null;
+  if (isLoading) return <Spinner />;
 
   return (
     <List>
