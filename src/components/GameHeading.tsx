de0692ec6efdd9f8f5757bf79cd4610d9e14a039
jebarcha <jebarcha@hotmail.com>
2023-09-23 17:41:38 -0600

    add my name to title

--- src/components/GameHeading.tsx
+++ src/components/GameHeading.tsx
@@ -8,7 +8,7 @@ interface Props {
 const GameHeading = ({ gameQuery }: Props) => {
   const heading = `${gameQuery.platform?.name || ''} ${
     gameQuery.genre?.name || ''
-  } Games`;
+  } Demo by Jose Barajas Ch - Games`;
   return (
     <Heading as={'h1'} marginY={5} fontSize='5xl'>
       {heading}
