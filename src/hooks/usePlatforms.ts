79cf8b64b9df0b67f6e104d149e460b2c51281a1
jebarcha <jebarcha@hotmail.com>
2023-10-18 18:05:14 -0600

    fix issue with queryKey

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,6 +1,6 @@
 //import useData from './useData';
 import { useQuery } from '@tanstack/react-query';
-import platform from '../data/platform';
+import platforms from '../data/platform';
 import APIClient from '../services/api-client';
 
 const apiClient = new APIClient<Platform>('/platforms/lists/parents');
@@ -19,7 +19,7 @@ const usePlatforms = () =>
     queryKey: ['platforms'],
     queryFn: apiClient.getAll,
     staleTime: 24 * 60 * 60 * 1000, //24h
-    initialData: { count: platform.length, results: platform },
+    initialData: platforms, //{ count: platform.length, results: platform },
   });
 
 export default usePlatforms;
