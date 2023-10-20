921d9ab38845e806c995619543125fbcf890ee43
jebarcha <jebarcha@hotmail.com>
2023-10-20 15:51:07 -0600

    Refactor-manage state with Zustand

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,25 +1,19 @@
 import {
+  Button,
   HStack,
+  Heading,
+  Image,
   List,
   ListItem,
-  Image,
-  Spinner,
-  Button,
-  Heading,
 } from '@chakra-ui/react';
-import useGenres, { Genre } from '../hooks/useGenres';
+import useGenres from '../hooks/useGenres';
 import getCroppedImageUrl from '../services/image-url';
+import useGameQueryStore from '../store';
 
-interface Props {
-  onSelectGenre: (genre: Genre) => void;
-  selectedGenreId?: number;
-}
-
-const GenreList = ({ selectedGenreId, onSelectGenre }: Props) => {
+const GenreList = () => {
   const { data: genres, isLoading, error } = useGenres();
-
-  // if (error) return null;
-  // if (isLoading) return <Spinner />;
+  const selectedGenreId = useGameQueryStore((s) => s.gameQuery.genreId);
+  const setSelectedGenreId = useGameQueryStore((s) => s.setGenreId);
 
   return (
     <>
@@ -40,7 +34,7 @@ const GenreList = ({ selectedGenreId, onSelectGenre }: Props) => {
                 whiteSpace='normal'
                 textAlign='left'
                 fontWeight={genre.id == selectedGenreId ? 'bold' : 'normal'}
-                onClick={() => onSelectGenre(genre)}
+                onClick={() => setSelectedGenreId(genre.id)}
                 fontSize='lg'
                 variant='link'
               >
